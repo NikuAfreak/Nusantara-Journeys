cb60a56ede72e7d46ed9eab2ba2ca72a82a05cb9
nubhanamin <nubhanamin@gmail.com>
2025-06-12 00:06:48 +0800

    add sumtin

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PackageController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CheckoutController;
 
 // Home page route
 Route::get('/', function () {
